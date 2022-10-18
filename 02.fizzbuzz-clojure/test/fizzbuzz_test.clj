c3d78e9f7b4ae8efbb7958cd193cc8df58fdb680
RamonAranda <ramon.aranda.r@gmail.com>
2022-10-18 15:57:05 +0200

    Stage 1

--- 02.fizzbuzz-clojure/test/fizzbuzz_test.clj
+++ 02.fizzbuzz-clojure/test/fizzbuzz_test.clj
@@ -3,6 +3,25 @@
             [fizzbuzz :refer [fizzbuzz-algorithm]]))
 
 (deftest stage1
-  (testing "Sample test"
+  (testing "Numbers that are non divisible by 3 and 5 return the number as is"
     (are [number] (= number (fizzbuzz-algorithm number))
-                  1)))
+                  1
+                  2
+                  4
+                  7
+                  8))
+  (testing "Numbers that are divisible by 3 return Fizz"
+    (are [number] (= "Fizz" (fizzbuzz-algorithm number))
+                  3
+                  6
+                  9
+                  12))
+  (testing "Numbers that are divisible by 5 return Buzz"
+    (are [number] (= "Buzz" (fizzbuzz-algorithm number))
+                  5
+                  10
+                  20))
+  (testing "Numbers that are divisible by 3 and 5 return FizzBuzz"
+    (are [number] (= "FizzBuzz" (fizzbuzz-algorithm number))
+                  15
+                  30)))
