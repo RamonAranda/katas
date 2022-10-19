f57b83974681ff661550ce4cc96155664651018d
RamonAranda <ramon.aranda.r@gmail.com>
2022-10-19 12:11:23 +0200

    First iteration

--- 03.rock-paper-scissors/src/rock_paper_scissors.clj
+++ 03.rock-paper-scissors/src/rock_paper_scissors.clj
@@ -2,4 +2,10 @@
 
 (defn rock-paper-scissors-game
   [player1 player2]
-  "Draw!")
+  (cond
+    (= player1 player2) "Draw!"
+    (and (= player1 "Scissors") (= player2 "Paper")) "Player 1 wins!"
+    (and (= player1 "Rock") (= player2 "Scissors")) "Player 1 wins!"
+    (and (= player1 "Paper") (= player2 "Rock")) "Player 1 wins!"
+    :else "Player 2 wins!"
+    ))
--- 03.rock-paper-scissors/test/rock_paper_scissors_test.clj
+++ 03.rock-paper-scissors/test/rock_paper_scissors_test.clj
@@ -1,8 +1,20 @@
-(ns rock-paper-scissors_test
+(ns rock-paper-scissors-test
   (:require [clojure.test :refer [are deftest testing]]
             [rock-paper-scissors :refer [rock-paper-scissors-game]]))
 
-(deftest RockScissorsPaperGame
-  (testing "foo"
+(deftest rock-paper-scissors-game-test
+  (testing "Players draw if they play the same shape"
     (are [player1 player2] (= "Draw!" (rock-paper-scissors-game player1 player2))
-                  "Scissors" "Scissors")))
+                  "Scissors" "Scissors"
+                  "Paper" "Paper"
+                  "Rock" "Rock"))
+  (testing "Player 1 beats Player 2"
+    (are [player1 player2] (= "Player 1 wins!" (rock-paper-scissors-game player1 player2))
+                  "Scissors" "Paper"
+                  "Paper" "Rock"
+                  "Rock" "Scissors"))
+  (testing "Player 2 beats Player 1"
+    (are [player1 player2] (= "Player 2 wins!" (rock-paper-scissors-game player1 player2))
+                  "Paper" "Scissors"
+                  "Rock" "Paper"
+                  "Scissors" "Rock")))
