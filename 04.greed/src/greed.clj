5a46e68f8bd6ebe82a6f66270814ccf558b2becd
RamonAranda <ramon.aranda.r@gmail.com>
2022-10-19 15:58:03 +0200

    First approach for 1 and 5 dice scores

--- 04.greed/src/greed.clj
+++ 04.greed/src/greed.clj
@@ -1,5 +1,22 @@
 (ns greed)
 
+; See frequencies
+;(defn group_by_count
+;  [values]
+;  (->>
+;    (group-by identity values)
+;    (map (fn [[key values]] [key (count values)]))
+;    (into {})
+;    )
+;  )
+
 (defn greed-game
   [dices]
-  0)
+  (let [grouped-values (frequencies dices)
+        contains-1 (contains? grouped-values 1)
+        contains-5 (contains? grouped-values 5)]
+    (cond
+      (and contains-5 contains-1) 150
+      contains-5 50
+      contains-1 100
+      :else 0)))
--- 04.greed/test/greed_test.clj
+++ 04.greed/test/greed_test.clj
@@ -3,6 +3,24 @@
             [greed :refer [greed-game]]))
 
 (deftest greed-game-test
-  (testing "Foo"
+  (testing "Scores no point"
     (are [dices] (= (greed-game dices) 0)
-                 0)))
+                 [2 3 4 2 6]
+                 [2 3 4 6]
+                 [2 3 4 6]))
+  (testing "Scores a single one"
+    (are [dices] (= (greed-game dices) 100)
+                 [1 2 3 4 2 6]
+                 [2 3 1 4 6]
+                 [2 3 4 6 1]))
+  (testing "Scores a single five"
+    (are [dices] (= (greed-game dices) 50)
+                 [5 2 3 4 6]
+                 [2 3 5 4 6]
+                 [2 3 4 6 5]))
+  (testing "Score is accumulated for a one and a five"
+    (are [dices] (= (greed-game dices) 150)
+                 [1 5 2 3 4 6]
+                 [1 2 3 5 4 6]
+                 [1 2 3 4 6 5]
+                 [2 3 4 1 6 5])))
