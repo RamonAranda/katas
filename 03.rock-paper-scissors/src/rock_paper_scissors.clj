b787b1c8c32cba0ea7e62f14d746fa6ce8c62411
RamonAranda <ramon.aranda.r@gmail.com>
2022-10-19 12:40:32 +0200

    Refactor to reduce branches

--- 03.rock-paper-scissors/src/rock_paper_scissors.clj
+++ 03.rock-paper-scissors/src/rock_paper_scissors.clj
@@ -2,10 +2,11 @@
 
 (defn rock-paper-scissors-game
   [player1 player2]
-  (cond
-    (= player1 player2) "Draw!"
-    (and (= player1 "Scissors") (= player2 "Paper")) "Player 1 won!"
-    (and (= player1 "Rock") (= player2 "Scissors")) "Player 1 won!"
-    (and (= player1 "Paper") (= player2 "Rock")) "Player 1 won!"
-    :else "Player 2 won!"
-    ))
+  (let [shape-pair-win-options #{["Scissors" "Paper"] ["Rock" "Scissors"] ["Paper" "Rock"]}
+        shape-pair [player1 player2]]
+    (cond
+      (= player1 player2) "Draw!"
+      (contains? shape-pair-win-options shape-pair) "Player 1 won!"
+      :else "Player 2 won!"
+      ))
+  )
