509cf3fb2d92c9c4e1868d61840a5936c992d450
RamonAranda <ramon.aranda.r@gmail.com>
2022-10-19 12:23:06 +0200

    Little test refactor

--- 03.rock-paper-scissors/test/rock_paper_scissors_test.clj
+++ 03.rock-paper-scissors/test/rock_paper_scissors_test.clj
@@ -4,17 +4,19 @@
 
 (deftest rock-paper-scissors-game-test
   (testing "Players draw if they play the same shape"
-    (are [player1 player2] (= "Draw!" (rock-paper-scissors-game player1 player2))
-                  "Scissors" "Scissors"
-                  "Paper" "Paper"
-                  "Rock" "Rock"))
-  (testing "Player 1 beats Player 2"
-    (are [player1 player2] (= "Player 1 won!" (rock-paper-scissors-game player1 player2))
-                  "Scissors" "Paper"
-                  "Paper" "Rock"
-                  "Rock" "Scissors"))
-  (testing "Player 2 beats Player 1"
-    (are [player1 player2] (= "Player 2 won!" (rock-paper-scissors-game player1 player2))
-                  "Paper" "Scissors"
-                  "Rock" "Paper"
-                  "Scissors" "Rock")))
+    (are [player1 player2] (= (rock-paper-scissors-game player1 player2) "Draw!")
+                           "Scissors" "Scissors"
+                           "Paper" "Paper"
+                           "Rock" "Rock"))
+  (testing "Scissor beats Paper"
+    (are [player1 player2 expected] (= (rock-paper-scissors-game player1 player2) expected)
+                                    "Scissors" "Paper" "Player 1 won!"
+                                    "Paper" "Scissors" "Player 2 won!"))
+  (testing "Paper beats Rock"
+    (are [player1 player2 expected] (= (rock-paper-scissors-game player1 player2) expected)
+                                    "Paper" "Rock" "Player 1 won!"
+                                    "Rock" "Paper" "Player 2 won!"))
+  (testing "Rock beats Scissors"
+    (are [player1 player2 expected] (= (rock-paper-scissors-game player1 player2) expected)
+                                    "Rock" "Scissors" "Player 1 won!"
+                                    "Scissors" "Rock" "Player 2 won!")))
